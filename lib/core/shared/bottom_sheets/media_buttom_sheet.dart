29c73bc9dc7b9e9dea801af9973c6ddef2aee076
Mohamed Hassan <mohamed7github@gmail.com>
2025-03-18 16:20:39 +0200

    Update media button sheet to support dark mode and adjust padding

--- lib/core/shared/bottom_sheets/media_buttom_sheet.dart
+++ lib/core/shared/bottom_sheets/media_buttom_sheet.dart
@@ -3,13 +3,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:t_store_admin_panel/config/service_locator/service_locator.dart';
 import 'package:t_store_admin_panel/core/utils/utils/constants/colors.dart';
 import 'package:t_store_admin_panel/core/utils/utils/constants/sizes.dart';
+import 'package:t_store_admin_panel/core/utils/utils/helpers/helper_functions.dart';
 import 'package:t_store_admin_panel/features/media/cubits/media/media_cubit.dart';
 import 'package:t_store_admin_panel/features/media/presentation/widget/media_content/media_content_section.dart';
 import 'package:t_store_admin_panel/features/media/presentation/widget/media_uploader/media_uploader_section.dart';
 
 void showMediaButtomSheet(BuildContext context) {
+  final isDark = HelperFunctions.isDarkMode(context);
   showBottomSheet(
-    backgroundColor: AppColors.primaryBackground,
+    backgroundColor: isDark ? Colors.black : AppColors.primaryBackground,
     context: context,
     builder: (context) {
       return BlocProvider.value(
@@ -18,7 +20,7 @@ void showMediaButtomSheet(BuildContext context) {
           heightFactor: 1, // that mean take all screen height
           child: SingleChildScrollView(
             child: Padding(
-              padding: EdgeInsets.all(AppSizes.defaultSpace),
+              padding: EdgeInsets.all(AppSizes.spaceBtwItems),
               child: Column(
                 children: [
                   // Media Uploader Section
