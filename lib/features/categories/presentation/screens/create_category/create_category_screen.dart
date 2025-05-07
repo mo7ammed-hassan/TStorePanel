3074ed2baea7b12ae646a5cc08af1f029295e34a
Mohamed Hassan <mohamed7github@gmail.com>
2025-05-07 23:40:16 +0300

    refactor: replace ResponsiveLayouts with SiteTemplate in CreateCategoryScreen

--- lib/features/categories/presentation/screens/create_category/create_category_screen.dart
+++ lib/features/categories/presentation/screens/create_category/create_category_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:t_store_admin_panel/config/service_locator/service_locator.dart';
-import 'package:t_store_admin_panel/core/shared/widgets/responsive/responsive_layouts.dart';
+import 'package:t_store_admin_panel/core/shared/widgets/layouts/templates/site_layout.dart';
 import 'package:t_store_admin_panel/features/categories/cubits/category/category_cubit.dart';
 import 'package:t_store_admin_panel/features/categories/cubits/create_category/create_category_cubit.dart';
 import 'package:t_store_admin_panel/features/categories/presentation/screens/create_category/responsive_screens/create_category_desktop_screen.dart';
@@ -16,7 +16,7 @@ class CreateCategoryScreen extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as CategoryCubit;
     return BlocProvider(
       create: (context) => getIt<CreateCategoryCubit>(),
-      child: ResponsiveLayouts(
+      child: SiteTemplate(
         desktop: CreateCategoryDesktopScreen(categoryCubit: categoryCubit),
         tablet: CreateCategoryMobileScreen(categoryCubit: categoryCubit),
         mobile: CreateCategoryMobileScreen(categoryCubit: categoryCubit),
